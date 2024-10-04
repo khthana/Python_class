47110377ad6d03cec5941708a3ae12a347d06684
Thana Hongsuwan <khthana@hotmail.com>
2024-10-04 10:54:15 +0700

    Delete chapter_05 directory

--- chapter_05/temp.py
+++
@@ -1,9 +0,0 @@
-from enum import IntEnum
-
-class Action(IntEnum):
-    Rock = 0
-    Paper = 1
-    Scissors = 2
-
-choices = [f"{action.name}[{action.value}]" for action in Action]
-print (choices)
